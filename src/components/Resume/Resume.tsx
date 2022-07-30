4edc1f5906484c70be99058c7a4f32b474c9900e
Luis Pedro Carvalho Rodrigues <a42597@alunos.ipb.pt>
2022-07-30 18:01:21 +0100

    Build mock buttons

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -16,4 +16,4 @@ function Resume() {
     )
 }
 
-export default Resume
+export default Resume 
--- src/pages/Schedules/Schedules.tsx
+++ src/pages/Schedules/Schedules.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Button from '../../components/Button/Button'
 import Resume from '../../components/Resume/Resume'
+import SubTitle from '../../components/SubTitle/SubTitle'
 
-export default function Schedules() {
-
-    
-
-
+export default function Schedules() { 
+    const buttons = [{label: "20:00"},
+                    {label: "21:00"},
+                    {label: "19:00"}, 
+                    {label: "18:00"}, 
+                    {label: "17:00"},]
 
   return (
     <div>
         <Header/>
         <Resume/>
-
+        <SubTitle sentence='Escolha a hora: '/>
+        {buttons.map((button, index) => {
+            return(
+                <Button text={button.label} value={ 1 } selected={false} onClick={ () =>{} }/>
+            )
+        })}
     </div>
   )
 }
