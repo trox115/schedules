d1f751e8e10a42108df7b200aab66dad2143bcb0
António Fernandes <antoniofernandes@MBP-de-Antonio.Home>
2022-08-03 08:17:28 +0100

    create api functions

--- src/components/InitialScreen/InitialScreen.tsx
+++ src/components/InitialScreen/InitialScreen.tsx
@@ -1,11 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import logo from '../../logo.svg';
 import './InitialScreen.scss';
 import AppContext from '../../Context/App.context';
+import { useDispatch } from 'react-redux';
 
 function InitialScreen() {
   const { setPage } = useContext(AppContext);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch.content.getContent();
+  },[])
 
   return (
     <div className='initial'>
