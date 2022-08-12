57594fbf2d235676c8f754b35023636757b8702b
António Fernandes <antoniofernandes@MBP-de-Antonio.lan>
2022-08-12 21:41:07 +0100

    create function so buttons can adapt to selected time block

--- src/pages/Schedules/Schedules.tsx
+++ src/pages/Schedules/Schedules.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 import "./Schedules.scss"
 import Header from '../../components/Header/Header'
@@ -8,22 +9,45 @@ import SubTitle from '../../components/SubTitle/SubTitle'
 import AppContext from '../../Context/App.context'
 import StepBar from '../../components/StepBar/StepBar'
 import ArrowButton from '../../components/Button/ArrowButton'
+import { RootState } from '../../store'
+import { isWeekend, parse } from 'date-fns'
 
 export default function Schedules() {
-  const { setTime, setCurrentStep, time, setPage } = useContext(AppContext)
-  const buttons = [{ label: "20:00", value: 20 },
-  { label: "21:00", value: 21 },
-  { label: "19:00", value: 19 },
-  { label: "18:00", value: 18 },
-  { label: "17:00", value: 17 },
-  { label: "16:00", value: 16 }]
+  const { setTime, setCurrentStep, time, setPage, date, duration } = useContext(AppContext)
+  const [buttons, setButtons] = useState([{ label: '20', value: '0' }]);
+  const { availableTimes } = useSelector((state: RootState) => state.content);
 
+  const generateArrayOfTime = (from:string, to:string ) => {
+    const start = parse(from, 'HH:mm', date || new Date());
+    const end =  parse(to, 'HH:mm', date || new Date());
+
+    const multiplier = duration || 15;
+    const aux = []
+    for(let i = start; i <= end; i = new Date(i.getTime() + multiplier * 60000)){
+      aux.push({label:`${ i.getHours() }:${ i.getMinutes() === 0 ? '00' : i.getMinutes()}`,
+       value:`${ i.getHours() }:${ i.getMinutes() === 0 ? '00' : i.getMinutes()}`});
+    }
+    setButtons(aux);
+  }
+  
+  useEffect(() => {
+    if(date){
+      const usableId = isWeekend(date) ? 2 : 1;
+      const availability = availableTimes.find( (time) => time.id === usableId)
+
+      if(availability){
+        generateArrayOfTime(availability.from, availability.to);
+      }
+
+    }
+  },[date])
+  
   const onClick = (e: Event) => {
     e.preventDefault();
     const value = (e.target as HTMLButtonElement).value;
 
     if (setTime && setCurrentStep) {
-      setTime(+value);
+      setTime(value.toString());
       setCurrentStep(3);
     }
   }
