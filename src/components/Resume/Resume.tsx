b5633fa9c6adaa0d10f05e9e312298bdef9932c8
António Fernandes <antoniofernandes@MBP-de-Antonio.lan>
2022-08-12 21:40:19 +0100

    Update resume to remove extra string.

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react'
+import { format } from 'date-fns';
+import { pt } from 'date-fns/locale';
 
 import AppContext from '../../Context/App.context'
 import './Resume.scss';
@@ -11,8 +13,8 @@ function Resume() {
         <div className="resume">
           <span className='resume__text'>A sua seleção:</span>
           {duration && <span className='resume__text--bold'> { ` ${ duration } min` }</span>}
-          {date && <span className='resume__text--capitalize resume__text--bold'> {`| ${ date } `}</span>}
-          {time && <span className='resume__text--bold'> {`| ${ time }h00`}</span>}
+          {date && <span className='resume__text--capitalize resume__text--bold'> {`| ${ format(date, 'dd LLLL', { locale: pt }) } `}</span>}
+          {time && <span className='resume__text--bold'> {`| ${ time }`}</span>}
         </div>
     )
 }
