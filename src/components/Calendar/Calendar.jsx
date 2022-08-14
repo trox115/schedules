8029080aaa8a3772c4853d2cb6d2bf16a617c4b8
António Fernandes <antoniofernandes@MBP-de-Antonio.Home>
2022-08-14 16:20:38 +0100

    Check if date is available and dele day if it's not.

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import { addDays, isAfter } from "date-fns";
+import { useSelector } from "react-redux";
+import { format, formatISO } from 'date-fns';
+import _ from 'lodash';
 
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calendar.scss";
 import pt from 'date-fns/locale/pt';
 import AppContext from "../../Context/App.context";
 
-
 const Calendar = () => {
   const {setDate} = useContext(AppContext)
   const [startDate, setStartDate] = useState(addDays(new Date(), 1));
+  const { schedules } = useSelector((state) => state.schedules);
 
   useEffect(() => {
     setDate(startDate)    
@@ -18,8 +21,13 @@ const Calendar = () => {
 
   const isAvailable = (date) => {
     const today = new Date();
-    
-    return isAfter(date, today);
+    const formatedDate = format(date, 'dd/MM/yyyy');
+    const bookedDates = _.filter(schedules, (booked) => {
+      const splitedDate = (booked.date).split('T')
+      return format(new Date(splitedDate), 'dd/MM/yyyy') === formatedDate;
+    })
+
+    return isAfter(date, today) && bookedDates.length < 5;
   }
   
   return (
